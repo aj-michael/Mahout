e756bc73aa5ee238bf97b3db1c272c945a47deb3
Adam Michael <ajmichael0@gmail.com>
2014-11-02 00:54:38 -0400

    replaced identity mapper with filter to catch bad input

--- FromScratch/src/main/java/edu/rosehulman/ModelGenerator.java
+++ FromScratch/src/main/java/edu/rosehulman/ModelGenerator.java
@@ -12,15 +12,20 @@ import org.apache.hadoop.util.ToolRunner;
 import com.datasalt.pangool.io.Fields;
 import com.datasalt.pangool.io.ITuple;
 import com.datasalt.pangool.io.Schema;
+import com.datasalt.pangool.io.Tuple;
 import com.datasalt.pangool.tuplemr.IdentityTupleMapper;
 import com.datasalt.pangool.tuplemr.IdentityTupleReducer;
 import com.datasalt.pangool.tuplemr.TupleMRBuilder;
+import com.datasalt.pangool.tuplemr.TupleMapper;
 import com.datasalt.pangool.tuplemr.TupleReducer;
 import com.datasalt.pangool.tuplemr.mapred.lib.input.TupleTextInputFormat;
 
 public class ModelGenerator implements Tool, Serializable {
 
 	private static final long serialVersionUID = -5565331847345488539L;
+
+	static Schema line_schema = new Schema("line schema",
+			Fields.parse("line: string"));
 	
 	static Schema input_schema = new Schema("first schema",
 			Fields.parse("word: string, year: int, count: int, dcount: int"));
@@ -42,6 +47,33 @@ public class ModelGenerator implements Tool, Serializable {
 		Configuration conf = new Configuration();
 		TupleMRBuilder job = new TupleMRBuilder(conf, "Model Generator");
 		job.addIntermediateSchema(input_schema);
+		TupleMapper<ITuple,NullWritable> mapper = new TupleMapper<ITuple,NullWritable>() {
+			private static final long serialVersionUID = -5833328716962384147L;
+			@Override
+			public void map(ITuple key, NullWritable value,
+					TupleMapper<ITuple, NullWritable>.TupleMRContext context,
+					com.datasalt.pangool.tuplemr.TupleMapper.Collector collector)
+					throws IOException, InterruptedException {
+				String line = (String)key.get("line");
+				try {
+					String[] tokens = line.split("\t");
+					String word = tokens[0];
+					int year = Integer.parseInt(tokens[1]);
+					int count = Integer.parseInt(tokens[2]);
+					int dcount = Integer.parseInt(tokens[3]);
+					ITuple outTuple = new Tuple(input_schema);
+					outTuple.set("word", word);
+					outTuple.set("year", year);
+					outTuple.set("count", count);
+					outTuple.set("dcount",dcount);
+					collector.write(outTuple);
+				} catch (Exception e) {
+					// do nothing
+				}
+				
+			}
+			
+		};
 		TupleReducer<ITuple, NullWritable> reducer = new TupleReducer<ITuple, NullWritable>() {
 			private static final long serialVersionUID = -3998304148648759758L;
 
@@ -58,9 +90,14 @@ public class ModelGenerator implements Tool, Serializable {
 				collector.write(outputTuple, NullWritable.get());
 			}
 		};
+		job.addInput(new Path(input), new TupleTextInputFormat(line_schema,
+				false, false, TupleTextInputFormat.NO_SEPARATOR_CHARACTER, TupleTextInputFormat.NO_QUOTE_CHARACTER, TupleTextInputFormat.NO_ESCAPE_CHARACTER, null, "\\NULL"),
+				mapper);
+		/*
 		job.addInput(new Path(input), new TupleTextInputFormat(input_schema,
 				false, false, '\t', TupleTextInputFormat.NO_QUOTE_CHARACTER, TupleTextInputFormat.NO_ESCAPE_CHARACTER, null, "\\NULL"),
 				new IdentityTupleMapper());
+		*/
 		reducer = new IdentityTupleReducer();
 		job.setTupleCombiner(reducer);
 		job.setTupleReducer(reducer);
